063da2953ff9721955ec88c9e8513df85b0622fd
Roy Chen <asdfgh2046@gmail.com>
2015-12-18 01:46:24 +0800

    在onResume寫讀tag payload的程式碼

--- SimpleNFCRead/app/src/main/java/com/example/roy/simplenfcread/MainActivity.java
+++ SimpleNFCRead/app/src/main/java/com/example/roy/simplenfcread/MainActivity.java
@@ -1,13 +1,77 @@
 package com.example.roy.simplenfcread;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText myNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myNote = ((EditText) findViewById(R.id.editText));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            NdefMessage[] messages = getNdefMessages(getIntent());
+            byte[] payload = messages[0].getRecords()[0].getPayload();
+            setNoteBody(new String(payload));
+            setIntent(new Intent());
+        }
+    }
+
+
+    NdefMessage[] getNdefMessages(Intent intent) {
+        // Parse the intent
+        NdefMessage[] msgs = null;
+        String action = intent.getAction();
+
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+
+            Parcelable[] rawMsgs =
+                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            } else {
+                byte[] empty = new byte[] {};
+                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
+                NdefMessage msg = new NdefMessage(new NdefRecord[] {
+                        record
+                });
+                msgs = new NdefMessage[] {
+                        msg
+                };
+            }
+        } else {
+            Log.d("TAG", "Unknown intent.");
+            finish();
+        }
+        return msgs;
+    }
+
+    private void setNoteBody(String body) {
+        Editable text = myNote.getText();
+        text.clear();
+        text.append(body);
     }
 }
